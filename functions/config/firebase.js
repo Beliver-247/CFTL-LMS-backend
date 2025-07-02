f1179ec5c6b1d5aa65beb6cb3b20897ad692fd02
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2025-07-02 23:37:23 +0530

    removed secrects

--- functions/config/firebase.js
+++ functions/config/firebase.js
@@ -1,10 +1,19 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  // Use local service account only if running locally
+  if (process.env.FUNCTIONS_EMULATOR === "true" || process.env.NODE_ENV === "development") {
+    const serviceAccount = require("../serviceAccountKey.json");
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  } else {
+    // Automatically use Firebase credentials when deployed
+    admin.initializeApp();
+  }
+}
 
 const db = admin.firestore();
 
-module.exports = { db,admin };   
+module.exports = { db, admin };
