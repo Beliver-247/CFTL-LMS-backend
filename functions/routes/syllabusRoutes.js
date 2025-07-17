aafbc25d2ab306ea210ea8bf11ea898f0876d864
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2025-07-17 23:18:56 +0530

    syllabus backend done.

--- functions/routes/syllabusRoutes.js
+++ functions/routes/syllabusRoutes.js
@@ -4,13 +4,34 @@ const {
   getAllSyllabus,
   updateSyllabus,
   deleteSyllabus,
+  getSyllabusByCourseAndMonth,
+  markSubtopicComplete,
+  approveSyllabusChanges
 } = require("../controllers/syllabusController");
+
+const { verifyFirebaseToken } = require("../middleware/firebaseAuth");
 
 const router = express.Router();
 
-router.post("/", createSyllabus);
-router.get("/", getAllSyllabus);
-router.put("/:id", updateSyllabus);
-router.delete("/:id", deleteSyllabus);
+// Admin & General
+router.post("/", verifyFirebaseToken, createSyllabus);
+router.get("/", verifyFirebaseToken, getAllSyllabus);
+router.get("/:courseId/:month", verifyFirebaseToken, getSyllabusByCourseAndMonth);
+router.put("/:id", verifyFirebaseToken, updateSyllabus);
+router.delete("/:id", verifyFirebaseToken, deleteSyllabus);
+
+// Teacher: Mark a subtopic as complete
+router.patch(
+  "/:id/weeks/:weekNumber/topics/:topicIndex/subtopics/:subIndex",
+  verifyFirebaseToken,
+  markSubtopicComplete
+);
+
+// Coordinator: Approve pending changes
+router.post(
+  "/:id/approval",
+  verifyFirebaseToken,
+  approveSyllabusChanges
+);
 
 module.exports = router;
